95c9a976f7110c86f1be5ad974c5d5edf520b25e
Lee SeungHeon <lsh328328@naver.com>
2023-06-23 14:34:46 +0900

    [Setting]: spring security setting
    SecurityFilterChain으로 security 세팅 정의
    H2-console security 제외

---
+++ src/main/java/io/propwave/tree/config/security/SecurityConfig.java
@@ -0,0 +1,30 @@
+package io.propwave.tree.config.security;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.method.configuration.EnableMethodSecurity;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.config.annotation.web.configurers.AbstractHttpConfigurer;
+import org.springframework.security.web.SecurityFilterChain;
+
+import static org.springframework.boot.autoconfigure.security.servlet.PathRequest.toH2Console;
+
+@EnableWebSecurity
+@EnableMethodSecurity
+@Configuration
+public class SecurityConfig {
+
+    @Bean
+    public SecurityFilterChain securityFilterChain(HttpSecurity http) throws Exception {
+        http
+                .authorizeHttpRequests(auth -> auth
+                        .requestMatchers(toH2Console()).permitAll()
+                )
+                .headers(AbstractHttpConfigurer::disable
+                )
+                .csrf(AbstractHttpConfigurer::disable
+                );
+        return http.build();
+    }
+}
