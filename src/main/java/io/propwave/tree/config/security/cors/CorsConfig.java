aa916973af32c55ea45fd15bfcf2c384992a315e
Lee SeungHeon <lsh328328@naver.com>
2023-07-10 13:29:41 +0900

    [Setting]: CORS 세팅 변경

--- src/main/java/io/propwave/tree/config/security/cors/CorsConfig.java
+++ src/main/java/io/propwave/tree/config/security/cors/CorsConfig.java
@@ -7,6 +7,7 @@ import org.springframework.web.cors.CorsConfigurationSource;
 import org.springframework.web.cors.UrlBasedCorsConfigurationSource;
 
 import java.util.Arrays;
+import java.util.List;
 
 @Configuration
 public class CorsConfig {
@@ -16,8 +17,12 @@ public class CorsConfig {
         CorsConfiguration config = new CorsConfiguration();
 
         config.setAllowCredentials(true);
-        config.setAllowedOrigins(Arrays.asList("http://localhost:3000", "https://3tree.io"));
-        config.setAllowedMethods(Arrays.asList("GET", "POST", "PUT", "DELETE", "PATCH", "OPTIONS"));
+        config.setAllowedOriginPatterns(List.of("*"));
+        config.addAllowedOrigin("http://localhost:3000");
+        config.addAllowedOrigin("https://3tree.io");
+        config.addAllowedMethod("*");
+        config.addAllowedHeader("*");
+        config.addExposedHeader("*");
 
         UrlBasedCorsConfigurationSource source = new UrlBasedCorsConfigurationSource();
         source.registerCorsConfiguration("/**", config);
