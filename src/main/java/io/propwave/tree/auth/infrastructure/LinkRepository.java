7477ba9bf81e4abd6a466eff257596ecf932b6bd
Lee SeungHeon <lsh328328@naver.com>
2023-07-12 13:19:04 +0900

    [Fix]: querydsl findAllByUserId 메서드 삭제

--- src/main/java/io/propwave/tree/auth/infrastructure/LinkRepository.java
+++ src/main/java/io/propwave/tree/auth/infrastructure/LinkRepository.java
@@ -3,15 +3,17 @@ package io.propwave.tree.auth.infrastructure;
 import io.propwave.tree.auth.domain.Link;
 import org.springframework.data.repository.Repository;
 
+import java.util.List;
 import java.util.Optional;
 
-public interface LinkRepository extends Repository<Link, Long>, LinkRepositoryCustom {
+public interface LinkRepository extends Repository<Link, Long> {
 
     // CREATE
     void save(Link link);
 
     // READ
     Optional<Link> findById(Long id);
+    List<Link> findAllByUserId(Long id);
 
     // UPDATE
 
